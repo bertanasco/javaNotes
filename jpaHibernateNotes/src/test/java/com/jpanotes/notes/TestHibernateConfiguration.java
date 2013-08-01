45d6f7cc8549a4bc2596090686fc6fa6a0a4ef68
bertanasco <bertanasco@gmail.com>
2013-08-01 16:25:20 +0800

    updates on log4j

--- jpaHibernateNotes/src/test/java/com/jpanotes/notes/TestHibernateConfiguration.java
+++ jpaHibernateNotes/src/test/java/com/jpanotes/notes/TestHibernateConfiguration.java
@@ -7,18 +7,24 @@ import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.jpanotes.model.User;
 
 public class TestHibernateConfiguration {
+	private static Logger log = LoggerFactory.getLogger(TestHibernateConfiguration.class);
 	private static Configuration config = null;
 	private static SchemaExport schemaExport  = null;
 
 	@BeforeClass
 	public static void setUp(){
+		log.info("creating a new configuration");
 		config = new Configuration();
 		config.addAnnotatedClass(User.class);
 		config.configure();
+		log.info("Done reading hbm configuration");
+		log.info("Creating schemaExport from configuration");
 		schemaExport = new SchemaExport(config);
 		schemaExport.create(true, true);
 	}
@@ -28,11 +34,13 @@ public class TestHibernateConfiguration {
 	}
 	@Test
 	public void testConfiguration(){
+		log.info("Testing schema config");
 		assertNotNull(config);
 	}
 
 	@Test
 	public void testSchemaExport(){
+		log.info("Testing schema export");
 		assertNotNull(schemaExport);
 		//schemaExport.
 	}
